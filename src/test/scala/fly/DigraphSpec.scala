c155cc9c1b6017bcc739b92dbbda4ecfafb45597
vadym.oliinyk <vadym.oliinyk@wgu.edu>
2017-08-20 14:52:18 -0600

    Add unit tests for graph traversal

--- src/test/scala/fly/DigraphSpec.scala
+++ src/test/scala/fly/DigraphSpec.scala
@@ -1,4 +1,5 @@
 package fly
+import scala.collection.immutable
 
 class DigraphSpec extends UnitSpec {
 
@@ -10,21 +11,76 @@ class DigraphSpec extends UnitSpec {
   }
 
   private def emptyDigraph[T] = EmptyDigraph[T]()
-  private def paths[T](g: Digraph[T], source: T) = pathsTailRecursive(g, source, 1)
 
-  private val graph2Line = emptyDigraph.addEdge(1, 2).addEdge(2, 3)
-  private val graph4Tree = emptyDigraph.addEdge(1, 2).addEdge(2, 3).addEdge(3, 4).addEdge(1, 4)
-  private val graph4TreeAlt = emptyDigraph.addEdge(1, 4).addEdge(3, 4).addEdge(2, 3).addEdge(1, 2)
-  private val graph4TreeCycle = emptyDigraph.addEdge(1, 4).addEdge(3, 4).addEdge(2, 3).addEdge(1, 2).addEdge(4, 1)
+  private val Array(a_, b, c, d, e) = Array('a', 'b', 'c', 'd', 'e')
 
-  "A graph" should "contain return 2 paths" in {
-    assert(paths(graph2Line, 1).size == 3)
-    assert(paths(graph2Line, 2).size == 2)
-    assert(paths(graph4Tree, 1).size == 5)
-    assert(paths(graph4Tree, 2).size == 3)
-    assert(paths(graph4TreeAlt, 1).size == 5)
-    assert(paths(graph4TreeAlt, 2).size == 3)
-    assert(paths(graph4TreeCycle, 1).size == 5)
-    assert(paths(graph4TreeCycle, 2).size == 4)
+  private def paths[T](g: Digraph[T], source: T)(implicit depth: Int = 5) = pathsTailRecursive(g, source, depth)
+  // a -> b
+  private val graphSingleEdge = emptyDigraph[Char].addEdge(a_, b)
+
+  // a -> b -> c
+  private val graph2Linear = emptyDigraph[Char].addEdge(a_, b).addEdge(b, c)
+
+  /**
+    a->b->c->d
+     ⤷     ⤴
+  */
+  private val graph4Tree = emptyDigraph[Char].addEdge(a_, b).addEdge(b, c).addEdge(c, d).addEdge(a_, d)
+
+  /** a->d<-c
+      ⤷ b ⤴
+  */
+  private val graph4TreeAlt = emptyDigraph[Char].addEdge(a_, d).addEdge(c, d).addEdge(b, c).addEdge(a_, b)
+
+  /** a⟺d<-c
+      ⤷ b ⤴
+  */
+  private val graph4TreeCycled = emptyDigraph[Char]
+    .addEdge(a_, d)
+    .addEdge(c, d)
+    .addEdge(b, c)
+    .addEdge(a_, b)
+    .addEdge(d, a_)
+
+
+  "paths" should "return 0 paths for empty graph" in {
+    assert(paths(emptyDigraph[Char], a_).isEmpty)
+  }
+
+  it should "return 1 path in single edge graph" in {
+    val ps: immutable.Seq[List[Char]] = paths(graphSingleEdge, a_)
+    assert(ps.size == 1)
+    assert(ps.contains(List(b, a_)))
+  }
+
+  it should "return 0 paths in a single edge graph if source has 0 out degree" in {
+    assert(paths(graphSingleEdge, b).isEmpty)
+  }
+
+  it should "return 0 paths in a single edge graph if source is not in the graph" in {
+    assert(paths(graphSingleEdge, c).isEmpty)
+  }
+
+  it should "return correct paths" in {
+    assert(paths(graph2Linear, a_).size == 2)
+    assert(paths(graph2Linear, b).size == 1)
+    assert(paths(graph4Tree, a_).size == 4)
+    assert(paths(graph4Tree, b).size == 2)
+    assert(paths(graph4TreeAlt, a_).size == 4)
+    assert(paths(graph4TreeAlt, b).size == 2)
+    assert(paths(graph4TreeCycled, a_).size == 4)
+    assert(paths(graph4TreeCycled, b).size == 3)
+  }
+
+  it should "return path with length <= specified length" in {
+    val edges = 2
+    val vertices = edges + 1
+    val ps = paths(graph4TreeCycled, a_)(edges)
+    ps.foreach(x => assert(x.size <= vertices))
+    assert(ps.size == 3)
+  }
+
+  private def assert[T](actualPaths: List[List[T]], expSize: Int, expectedPaths: List[List[T]]) = {
+
   }
 }
